b4b787e1ef7b3a3e568a9979ae19ee292e61d3ca
Alam-23 <muhammadnuralam23@gmail.com>
2025-03-18 13:50:11 +0700

    Update dark mode + update Fitur

--- js/script.js
+++ js/script.js
@@ -1,47 +1,56 @@
 //navbar fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+  const toTop = document.getElementById('toTop');
 
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed')
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed')
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
+  } else {
+    header.classList.remove('navbar-fixed')
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
+  }
 }
-
 
 //hambuerger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu')
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 })
 
 
-//carosel
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-const carousel = document.getElementById('carousel');
-let offset = 0;
-const slideWidth = document.querySelector('.swiper-slide').offsetWidth; // Mengambil lebar slide
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+})
 
-// Fungsi untuk geser ke kiri
-prevButton.addEventListener('click', () => {
-  if (offset > 0) {
-    offset -= slideWidth;
-    carousel.style.transform = `translateX(-${offset}px)`;
+// dark mode toggle
+const darkToggle = document.getElementById('dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = "light";
   }
 });
 
-// Fungsi untuk geser ke kanan
-nextButton.addEventListener('click', () => {
-  const totalSlides = document.querySelectorAll('.swiper-slide').length;
-  const maxOffset = slideWidth * (totalSlides - 3); // Menyesuaikan dengan 3 slide yang ditampilkan
-  if (offset < maxOffset) {
-    offset += slideWidth;
-    carousel.style.transform = `translateX(-${offset}px)`;
-  }
-});
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme === "dark" || (!currentTheme && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  document.documentElement.classList.add("dark");
+  darkToggle.checked = true; 
+} else {
+  document.documentElement.classList.remove("dark");
+  darkToggle.checked = false; 
+}
